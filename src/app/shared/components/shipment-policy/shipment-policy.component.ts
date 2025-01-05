b9efa2051ef8a5f9c8ac81ff9279f2605a20d427
aravindnaidu09 <aravindbabu372@gmail.com>
2025-01-05 22:55:17 +0530

    updates
    added policies(privacy, shipment, refund, terms)

---
+++ src/app/shared/components/shipment-policy/shipment-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipment-policy',
+  imports: [],
+  templateUrl: './shipment-policy.component.html',
+  styleUrl: './shipment-policy.component.scss'
+})
+export class ShipmentPolicyComponent {
+
+}
