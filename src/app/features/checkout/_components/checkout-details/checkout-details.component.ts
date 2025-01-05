145e56bd52a213416ceb724040dc7367ba847507
aravindnaidu09 <aravindbabu372@gmail.com>
2025-01-05 21:55:59 +0530

    updatest
    added basic template & necessary libraries

---
+++ src/app/features/checkout/_components/checkout-details/checkout-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-details',
+  imports: [],
+  templateUrl: './checkout-details.component.html',
+  styleUrl: './checkout-details.component.scss'
+})
+export class CheckoutDetailsComponent {
+
+}
