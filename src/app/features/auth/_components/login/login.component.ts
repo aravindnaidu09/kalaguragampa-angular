296673dcda8962b5087cf77a93e4e06d18c86ecd
aravindnaidu09 <aravindbabu372@gmail.com>
2025-01-11 13:30:45 +0530

    updates

--- src/app/features/auth/_components/login/login.component.ts
+++ src/app/features/auth/_components/login/login.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { SplitterModule } from 'primeng/splitter';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    SplitterModule,
+    DialogModule,
+    ButtonModule,
+    FormsModule
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  display: boolean = false;
+
+  onSubmit() {}
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,73 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch, faShoppingCart, faUser,  } from '@fortawesome/free-solid-svg-icons';
-
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectModule } from 'primeng/select';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
+import { LoginComponent } from "../../../features/auth/_components/login/login.component";
 @Component({
   selector: 'app-header',
-  imports: [FontAwesomeModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    FontAwesomeModule,
+    SelectModule,
+    MenuModule,
+    DialogModule,
+    LoginComponent
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   faSearch = faSearch;
   faUser = faUser;
-  faShoppingCart = faShoppingCart
+  faShoppingCart = faShoppingCart;
+
+  selectedLanguage: any = 1;
+
+  selectedCountry: string = 'India';
+  isDropdownOpen = false;
+
+  isAccountDropdownOpen: boolean = false;
+  menuItems: MenuItem[] = [];
+
+  showLoginDialog: boolean = false;
+
+  countryOptions: any = [
+    { id: 'in', name: 'India' },
+    { id: 'aus', name: 'Australia' },
+    { id: 'eur', name: 'Europe' },
+    { id: 'sing', name: 'Singapore' },
+    { id: 'uk', name: 'UK' },
+    { id: 'us', name: 'USA' },
+    { id: 'can', name: 'Canada' },
+  ];
+
+  ngOnInit() {
+    this.menuItems = [
+      {label: 'Login/Signup', command: () => this.showLoginDialog = true}
+    ];
+  }
+
+  onSelect(country: string) {
+    this.selectedCountry = country;
+    this.isDropdownOpen = false;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  toggleAccountDropdown() {
+    this.isAccountDropdownOpen = !this.isAccountDropdownOpen;
+  }
+
+  showLogin() {
+
+  }
 }
