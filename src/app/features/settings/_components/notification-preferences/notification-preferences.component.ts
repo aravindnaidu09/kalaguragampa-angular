8ba8d411a2069e9b00cf29e0630dd1b7d38c0869
aravindnaidu09 <aravindbabu372@gmail.com>
2025-04-07 13:43:43 +0530

    modified notification preference page

--- src/app/features/settings/_components/notification-preferences/notification-preferences.component.ts
+++ src/app/features/settings/_components/notification-preferences/notification-preferences.component.ts
@@ -2,12 +2,30 @@ import { Component, inject, signal } from '@angular/core';
 import { NotificationSettings } from '../../_model/notification-settings-model';
 import { ToastService } from '../../../../core/services/toast.service';
 import { NotificationService } from '../../_services/notification.service';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notification-preferences',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './notification-preferences.component.html',
-  styleUrl: './notification-preferences.component.scss'
+  styleUrl: './notification-preferences.component.scss',
+  animations: [
+    trigger('fadeStagger', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(10px)' }),
+            stagger(100, animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })))
+          ],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class NotificationPreferencesComponent {
   private notificationService = inject(NotificationService);
@@ -20,6 +38,15 @@ export class NotificationPreferencesComponent {
     orderUpdates: false,
     promotional: false
   });
+
+  notificationOptions: { key: keyof NotificationSettings; label: string }[] = [
+    { key: 'email', label: 'Receive Email Notifications' },
+    { key: 'sms', label: 'Receive SMS Notifications' },
+    { key: 'push', label: 'Receive Push Notifications' },
+    { key: 'orderUpdates', label: 'Order Updates' },
+    { key: 'promotional', label: 'Promotional Offers' }
+  ];
+
 
   ngOnInit(): void {
     this.fetchNotificationSettings();
