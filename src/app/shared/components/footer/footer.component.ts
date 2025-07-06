bfd484eae1bace2eff7e8e6d48cd1cb33083a391
Sharath Bhavaraju <89680328+sharath2601@users.noreply.github.com>
2025-07-06 19:19:42 +0530

    Ui Changes for header and footeer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+isScreenBetween996And400: boolean = false;
   currentYear = new Date().getFullYear();
 
   constructor(private readonly router: Router) {}
 
+  ngOnInit(): void {
+    this.checkScreenWidth();
+    window.addEventListener('resize', () => {
+      this.checkScreenWidth();
+    });
+  }
+ @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenWidth();
+  }
+
+  private checkScreenWidth() {
+    const width = window.innerWidth;
+    this.isScreenBetween996And400 = width <= 724 && width >= 400;
+  }
   goToPolicyPage(route: string) {
     this.router.navigate([route])
   }
